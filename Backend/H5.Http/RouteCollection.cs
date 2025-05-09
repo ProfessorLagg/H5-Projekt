e9a489a8de9699c1cf40562e734cbba3be17c46a
ProfessorLagg <futurecake2999@gmail.com>
2025-05-09 22:41:49 +0200

    Påbegyndt udvikling på custom data struktur til HTTP serverens route matching

---
+++ Backend/H5.Http/RouteCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H5.Http;
+public sealed class RouteMatcher {
+    public record class RouteNode {
+        public string RouteFragment = string.Empty;
+        public IRequestHandler? Handler { get; private set; } = null;
+        public readonly List<RouteNode> ChildNodes = new();
+    }
+
+    private RouteNode RootNode = new();
+
+}
