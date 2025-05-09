afae3c7529b0064dd880449bb29644e02c0ca876
ProfessorLagg <futurecake2999@gmail.com>
2025-05-09 20:14:16 +0200

    Begun adding unit testing

---
+++ Backend/H5.Testing/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace H5.Testing;
+
+internal class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Backend/H5.Testing/TestRunner.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H5.Testing;
+internal class TestRunner {
+}
