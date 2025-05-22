fde9abac5ed82f71c823b299371796fb6fb55a3b
ProfessorLagg <futurecake2999@gmail.com>
2025-05-22 16:21:03 +0200

    Removed unused code

--- Backend/H5.Utils/Utils/StreamUtils.cs
+++ Backend/H5.Utils/Utils/StreamUtils.cs
@@ -14,30 +14,4 @@ public static class StreamUtils {
 		}
 	}
 #nullable disable
-	/// <summary>Check that 2 streams contain exactly the same bytes</summary>
-	/// <remarks>Does not reset <see cref="Stream.Position"/> for <paramref name="streamA"/> and <paramref name="streamB"/></remarks>
-	/// <param name="streamA"></param>
-	/// <param name="streamB"></param>
-	/// <returns><see langword="true"/> if <paramref name="streamA"/> and <paramref name="streamB"/> contain exactly the same bytes</returns>
-	/// <exception cref="ArgumentException"></exception>
-	public static bool EqualContent(this Stream streamA, Stream streamB) {
-		if (streamA.Length != streamB.Length) return false;
-
-		if (streamA.CanSeek && streamB.CanSeek) {
-			streamA.Seek(0, SeekOrigin.Begin);
-			streamB.Seek(0, SeekOrigin.Begin);
-		}
-		else if (streamA.Position != streamB.Position) {
-			throw new ArgumentException("Streams must either be seekable or have the same starting position");
-		}
-
-		BufferedStream bufstreamA = new(streamA);
-		BufferedStream bufstreamB = new(streamB);
-
-
-		for (int i = 0; i < streamA.Length; i++) {
-			if (bufstreamA.ReadByte() != bufstreamB.ReadByte()) return false;
-		}
-		return true;
-	}
 }
