a826b463a9304f5d911104f1655d4871ee12b564
ProfessorLagg <futurecake2999@gmail.com>
2025-05-30 18:34:35 +0200

    Made the intersection check much more performant

--- Frontend/js/extentions.js
+++ Frontend/js/extentions.js
@@ -4,14 +4,7 @@
  * @returns true if any part of this DOMRect intersects any part of the other DOMRect
  */
 DOMRect.prototype.intersects = function (other) {
-
-    if (!(other instanceof DOMRect)) { return false; }
-
-    const testT = this.top >= other.top && this.top <= other.bottom
-    const testL = this.left >= other.left && this.left <= other.right;
-
-    const result = testT && testL;
-    return result;
+    return this.top >= other.top && this.top <= other.bottom && this.left >= other.left && this.left <= other.right;
 }
 
 /**
